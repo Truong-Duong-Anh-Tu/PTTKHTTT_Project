f3611bd6173ac121e0b8c9dbd9415ad7b1f8c6a5
KaCha <nnktran.0703@gmail.com>
2025-06-28 11:57:33 +0700

    KhanhTran_ADD_design Thêm lịch nhân viên

--- PTTKHTTTProject/Properties/Resources.Designer.cs
+++ PTTKHTTTProject/Properties/Resources.Designer.cs
@@ -183,6 +183,16 @@ namespace PTTKHTTTProject.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap search2 {
+            get {
+                object obj = ResourceManager.GetObject("search2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap test {
             get {
                 object obj = ResourceManager.GetObject("test", resourceCulture);
