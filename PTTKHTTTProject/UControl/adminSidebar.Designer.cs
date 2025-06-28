5e76623fc105b3c96b974c2761f0f056104e28d4
Trần Đức Trí <117420686+tridpt@users.noreply.github.com>
2025-06-28 09:33:36 +0700

    Fix loi

--- PTTKHTTTProject/UControl/adminSidebar.Designer.cs
+++ PTTKHTTTProject/UControl/adminSidebar.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // pictureBox1
             // 
-            pictureBox1.Image = Properties.Resources.logo_1;
+            pictureBox1.Image = Properties.Resources.logonew;
             pictureBox1.Location = new Point(3, 3);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(91, 91);
